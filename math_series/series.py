472d6f13b8cbccc725c1c9abc865b56c7df2bff4
Brendan Welzien <brendanwelzien@gmail.com>
2020-11-10 12:25:25 -0800

    test and function setup

---
+++ math_series/series.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+    
+
+
+
+
+
+
+
+def lucas(n):
+
+
+
+
+
+
+def sum_series(n, x=0, y=1):
---
+++ tests/test_series.py
@@ -0,0 +1,30 @@
+from math_series.series import fibonacci, lucas, sum_series
+
+def fibonacci_zero():
+  actual = fibonacci(0)
+  expected = 
+  assert actual == expected
+
+
+
+
+
+
+
+
+def lucas_zero():
+    actual = lucas(0)
+    expected = 
+    assert actual == expected
+
+
+
+
+def sum_series_zero():
+    actual = sum_series(0)
+    expected = 
+    assert actual == expected
+
+
+
+
