13d91a151c0f95ea930f6b40241769c0a58f3f8e
Brendan Welzien <brendanwelzien@gmail.com>
2020-11-10 12:55:07 -0800

    working on lucas

--- math_series/series.py
+++ math_series/series.py
@@ -1,17 +1,28 @@
-def fibonacci(n):
-    
+def fibonacci(n): # recursion 
+    if n < 0:
+        print('error')
+    elif n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+       return (fibonacci(n-1)+ fibonacci(n-2))
 
-
-
-
-
-
+print(fibonacci(1))
 
 def lucas(n):
+    z = [2, 1]
+    if n == 0 :
+        return z[0]
+    elif n == 1:
+        return z[1]
+    else:
+        for r in range(2, n+1):
+            a = z[0] + z[1]
+            z.append(a)
+            z[0] = z[1]
+            z[1] = a
+            return z[n]
 
-
-
-
-
-
-def sum_series(n, x=0, y=1):
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,29 +1,26 @@
-from math_series.series import fibonacci, lucas, sum_series
+from math_series.series import fibonacci, lucas
 
 def fibonacci_zero():
   actual = fibonacci(0)
-  expected = 
+  expected = 0
   assert actual == expected
 
-
-
-
-
-
-
+def fibonaaci_one():
+    actual = fibonaaci(1)
+    expected = 1
+    assert actual == expected
 
 def lucas_zero():
     actual = lucas(0)
-    expected = 
+    expected = 2
+    assert actual == expected
+
+def lucas_one()
+    actual = lucas(1)
+    expected = 1
     assert actual == expected
 
 
-
-
-def sum_series_zero():
-    actual = sum_series(0)
-    expected = 
-    assert actual == expected
 
 
 
